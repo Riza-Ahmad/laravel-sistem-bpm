99653fb8dfb078e15ea091344f02386ff1a9946e
fakhri ahmad <rizaahmadd123@gmail.com>
2025-01-25 18:40:34 +0700

    peraturan

--- config/cache.php
+++ config/cache.php
@@ -7,7 +7,7 @@ return [
     /*
     |--------------------------------------------------------------------------
     | Default Cache Store
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------------------
     |
     | This option controls the default cache store that will be used by the
     | framework. This connection is utilized if another isn't explicitly
